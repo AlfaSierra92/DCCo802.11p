ba91ccd83447776358b9e2179f8efb042df2993d
Antonio Solida <solida.antonio@gmail.com>
2024-03-03 19:28:41 +0100

    Reading iface data

--- wi.c
+++ wi.c
@@ -1,42 +1,125 @@
 #include <stdio.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-#include <netinet/in.h>
-#include <linux/wireless.h>
+#include <iwlib.h>
+
+#define IFACE "wlo1"
+#define MAX_BUF_SIZE 1024
+
+int rx_bytes() {
+    // Path to the rx_bytes file
+    //const char *file_path = "/sys/class/net/wlo1/statistics/rx_bytes";
+    char file_path[MAX_BUF_SIZE];
+    snprintf(file_path, MAX_BUF_SIZE, "/sys/class/net/%s/statistics/rx_bytes", IFACE);
+    
+    // Open the file for reading
+    int fd = open(file_path, O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the contents of the file
+    char buffer[MAX_BUF_SIZE];
+    ssize_t bytes_read = read(fd, buffer, MAX_BUF_SIZE - 1);
+    if (bytes_read == -1) {
+        perror("Error reading file");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Null-terminate the buffer
+    buffer[bytes_read] = '\0';
+
+    // Convert the read bytes to a long long integer
+    long long rx_bytes = atoll(buffer);
+
+    // Close the file
+    close(fd);
+
+    // Print the value of rx_bytes
+    printf("rx_bytes: %lld\n", rx_bytes);
+
+    return 0;
+}
+
+int rx_packets() {
+    // Path to the rx_bytes file
+    //const char *file_path = "/sys/class/net/wlo1/statistics/rx_bytes";
+    char file_path[MAX_BUF_SIZE];
+    snprintf(file_path, MAX_BUF_SIZE, "/sys/class/net/%s/statistics/rx_packets", IFACE);
+    
+    // Open the file for reading
+    int fd = open(file_path, O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Read the contents of the file
+    char buffer[MAX_BUF_SIZE];
+    ssize_t bytes_read = read(fd, buffer, MAX_BUF_SIZE - 1);
+    if (bytes_read == -1) {
+        perror("Error reading file");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Null-terminate the buffer
+    buffer[bytes_read] = '\0';
+
+    // Convert the read bytes to a long long integer
+    long long rx_bytes = atoll(buffer);
+
+    // Close the file
+    close(fd);
+
+    // Print the value of rx_bytes
+    printf("rx_packets: %lld\n", rx_bytes);
+
+    return 0;
+}
+
+int wireless_stats(){
+    int sockfd;
+    struct wireless_info info;
+    iwstats stats;
+    iwrange range;
+
+    // Apri un socket per ottenere le informazioni wireless
+    sockfd = iw_sockets_open();
+    if (sockfd < 0) {
+        perror("Errore nell'apertura del socket");
+        return 1;
+    }
+
+    int has_range = iw_get_range_info(sockfd, IFACE, &range);
+    // Ottieni le informazioni wireless per l'interfaccia specificata
+    if (iw_get_stats(sockfd, IFACE, &stats, &range, has_range) < 0) {
+        perror("Errore nell'ottenere le informazioni wireless");
+        iw_sockets_close(sockfd);
+        return 1;
+    }
+
+    // results in stats.qual.qual e stats.qual.level
+    // thanks to https://github.com/nhoad/python-iwlib/blob/master/iwlib/iwconfig.py
+
+    // Stampa le statistiche ottenute
+    printf("Statistiche wireless:\n");
+    printf("Qualità del segnale: %d/70\n", stats.qual.qual);
+    printf("Livello del segnale: %d dBm\n", (int8_t) stats.qual.level);
+    // Puoi stampare altre informazioni disponibili in info
+
+    // Chiudi il socket
+    iw_sockets_close(sockfd);
+}
 
 int main() {
-    int sock;
-    struct ifreq ifr;
-    struct iw_statistics stats;
-
-    // Apri un socket di controllo
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock == -1) {
-        perror("socket");
-        return 1;
+    while(1){
+        wireless_stats();
+        rx_bytes();
+        rx_packets();
+        printf("\n");
+        sleep(1);
     }
-
-    // Imposta il nome dell'interfaccia su cui ottenere le statistiche
-    strncpy(ifr.ifr_name, "wlo1", IFNAMSIZ - 1);
-
-    // Ottieni le statistiche wireless usando ioctl con SIOCGIWSTATS
-    if (ioctl(sock, SIOCGIWSTATS, &ifr) == -1) {
-        perror("ioctl");
-        close(sock);
-        return 1;
-    }
-
-    // Copia le statistiche ottenute nella struttura stats
-    memcpy(&stats, &ifr.ifr_data, sizeof(stats));
-
-    // Stampare le statistiche ottenute
-    printf("Statistiche wireless:\n");
-    printf("Pacchetti ricevuti: %u\n", stats.qual.qual);
-    printf("Pacchetti persi: %u\n", stats.qual.level);
-    // Altre statistiche possono essere stampate in modo simile
-
-    // Chiudi il socket di controllo
-    close(sock);
-
+    
     return 0;
 }
